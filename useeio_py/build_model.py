f420e723ce85056e3874c973876c9e42ecb90047
Sebastian Rowan <79868066+sebastianrowan@users.noreply.github.com>
2023-01-26 16:46:44 -0500

    add __init__ function to model class

--- useeio_py/build_model.py
+++ useeio_py/build_model.py
@@ -3,7 +3,6 @@
 import pandas as pd
 import numpy as np
 from classes import Model
-from initialize_model import initialize_model
 
 #TODO: Most or all of these functions can probably be wrapped into methods inside the Model class and called on __init__()
 
@@ -20,12 +19,19 @@ def build_model(model_name, config_paths = None):
     return: An EEIO model instance with complete components and attributes
     #' @export
     '''
-    model = initialize_model(model_name, config_paths)
-    model = load_io_data(model, config_paths)
-    model = load_and_build_satellite_tables(model)
-    model = load_and_build_indicators(model)
-    model = load_demand_vectors(model)
-    model = construct_EEIO_matrices(model)
+    #TODO: add these functions as methods in the model class.
+    model = Model(model_name, config_paths)
+    model.load_io_data()
+    model.load_and_build_satellite_tables()
+    model.load_and_build_indicators()
+    model.load_demand_vectors()
+    model.construct_EEIO_matrices()
+    # model = initialize_model(model_name, config_paths) #DONE
+    # model = load_io_data(model, config_paths)
+    # model = load_and_build_satellite_tables(model)
+    # model = load_and_build_indicators(model)
+    # model = load_demand_vectors(model)
+    # model = construct_EEIO_matrices(model)
     return(model)
 
 def construct_EEIO_matrices(model):
